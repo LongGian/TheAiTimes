d1c0ebe2b2fb630eb4b880c55c379a0c7f45e7b2
CrSandr8 <caru.alexro@gmail.com>
2023-05-24 16:56:37 +0200

    Removed smtp pss

--- newsletter.php
+++ newsletter.php
@@ -22,7 +22,7 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'theaitimesit@gmail.com';
-    $mail->Password = 'viavozthcudwcaem';
+    $mail->Password = '';
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
 
