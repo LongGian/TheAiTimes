956a70a75bc09eb50fdb8e41d3b10c8ac14459e6
CrSandr8 <caru.alexro@gmail.com>
2023-05-24 13:32:22 +0200

    Modificato newsletter

--- newsletter.php
+++ newsletter.php
@@ -5,6 +5,12 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+//Database connection and Query 
+$db = pg_connect("host=horton.db.elephantsql.com dbname=dxeyhugp user=dxeyhugp password=GrDEM1FdeRba7cH3KQT1MPajUGBWytj0");
+$query = 'SELECT email FROM users WHERE newsletter==true';
+$res = pg_query($db, $query);
+$row = pg_fetch_assoc($res);
+$people = $row['email'];
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
@@ -15,29 +21,23 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Username   = 'theaitimesit@gmail.com';                     //SMTP username
+    $mail->Password   = 'viavozthcudwcaem';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
-    //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    $mail->setFrom('theaitimesit@gmail.com', 'TheAiTimes');
+    while($row = pg_fetch_assoc($res)){
+        $mail->addAddress($row['email']);
+    }                
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'Nuove News!';
+    $mail->Body    = 'Ciao! Se hai ricevuto questa email vuol dire che probabilmente ci sono novita sul nostro sito <b>NO REPLY</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
